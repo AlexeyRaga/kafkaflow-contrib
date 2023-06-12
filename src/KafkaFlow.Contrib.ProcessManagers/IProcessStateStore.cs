8db080378bbfb21b8ee97480e8aa8955c3fc5424
Alexey Raga <alexey.raga@gmail.com>
2023-06-12 20:18:37 +1000

    Document the Persist method

--- src/KafkaFlow.Contrib.ProcessManagers/IProcessStateStore.cs
+++ src/KafkaFlow.Contrib.ProcessManagers/IProcessStateStore.cs
@@ -13,7 +13,15 @@ public sealed class OptimisticConcurrencyException : Exception
 
 public interface IProcessStateStore
 {
+    /// <summary>
+    /// Persists the given process state.
+    /// Checks if the "marker" matches before overwriting the existing state.
+    /// Throws <see cref="T:OptimisticConcurrencyException"/> if markers don't match, which means that another process
+    /// may have updated the state.
+    /// </summary>
+    /// <exception cref="OptimisticConcurrencyException">When the state marker doesn't match the expected one while overwriting</exception>
     ValueTask Persist(Type processType, Guid processId, MarkedState state);
+
     ValueTask<MarkedState> Load(Type processType, Guid processId);
 
     ValueTask Delete(Type processType, Guid processId);
