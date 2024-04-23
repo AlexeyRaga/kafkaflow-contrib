4f72442f5af9fb8719de6d39abb81a09fff23cab
RobIII <r.janssen@keensystems.eu>
2024-04-23 17:07:44 +0200

    Run init.db on startup. This ain't pretty but it works.

--- src/Contrib.KafkaFlow.Outbox.SqlServer/schema/0002.Table.sql
+++ src/Contrib.KafkaFlow.Outbox.SqlServer/schema/0002.Table.sql
@@ -1,15 +1,18 @@
 /* Table */
-CREATE TABLE [kafkaflowtest].[dbo].[outbox](
-	[sequence_id] [bigint] IDENTITY(1,1) NOT NULL,
-	[topic_name] [nvarchar](255) NOT NULL,
-	[partition] [int] NULL,
-	[message_key] [varbinary](max) NULL,
-	[message_headers] [nvarchar](max) NULL,
-	[message_body] [varbinary](max) NULL,
-	[date_added_utc] [datetime2] NOT NULL DEFAULT(SYSUTCDATETIME()),
-    [rowversion] [int] NOT NULL DEFAULT(1),
-	CONSTRAINT [PK_outbox] PRIMARY KEY CLUSTERED ([sequence_id] ASC),
-	CONSTRAINT [CK_headers_not_blank_or_empty] CHECK ((TRIM([message_headers])<>N'')),
-	CONSTRAINT [CK_topic_name_not_blank_or_empty] CHECK ((TRIM([topic_name])<>N''))
-)
+IF OBJECT_ID(N'[kafkaflowtest].[dbo].[outbox]', N'U') IS NULL
+BEGIN
+    CREATE TABLE [kafkaflowtest].[dbo].[outbox](
+	    [sequence_id] [bigint] IDENTITY(1,1) NOT NULL,
+	    [topic_name] [nvarchar](255) NOT NULL,
+	    [partition] [int] NULL,
+	    [message_key] [varbinary](max) NULL,
+	    [message_headers] [nvarchar](max) NULL,
+	    [message_body] [varbinary](max) NULL,
+	    [date_added_utc] [datetime2] NOT NULL DEFAULT(SYSUTCDATETIME()),
+        [rowversion] [int] NOT NULL DEFAULT(1),
+	    CONSTRAINT [PK_outbox] PRIMARY KEY CLUSTERED ([sequence_id] ASC),
+	    CONSTRAINT [CK_headers_not_blank_or_empty] CHECK ((TRIM([message_headers])<>N'')),
+	    CONSTRAINT [CK_topic_name_not_blank_or_empty] CHECK ((TRIM([topic_name])<>N''))
+    )
+END
 GO
