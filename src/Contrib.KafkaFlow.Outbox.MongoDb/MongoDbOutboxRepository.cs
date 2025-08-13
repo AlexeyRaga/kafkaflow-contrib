306fd20af8f08931507b5ddbfbfd56e12c7d0a72
Alexey Raga <alexey.raga@gmail.com>
2025-08-13 16:26:45 +1000

    Add locks to mongodb dispatcher

--- src/Contrib.KafkaFlow.Outbox.MongoDb/MongoDbOutboxRepository.cs
+++ src/Contrib.KafkaFlow.Outbox.MongoDb/MongoDbOutboxRepository.cs
@@ -7,13 +7,17 @@ namespace Contrib.KafkaFlow.Outbox.MongoDb;
 public class MongoDbOutboxRepository : IOutboxRepository
 {
     private readonly IMongoCollection<OutboxDocument> _collection;
+    private readonly IMongoCollection<LockDocument> _lockCollection;
     private readonly IMongoClient _client;
+    private readonly string _instanceId;
     private long _sequenceCounter;
 
     public MongoDbOutboxRepository(IMongoDatabase database, string collectionName = "outbox")
     {
         _collection = database.GetCollection<OutboxDocument>(collectionName);
+        _lockCollection = database.GetCollection<LockDocument>($"{collectionName}_locks");
         _client = database.Client;
+        _instanceId = Environment.MachineName + "-" + Environment.ProcessId + "-" + Guid.NewGuid().ToString("N")[..8];
 
         var indexKeys = Builders<OutboxDocument>.IndexKeys.Ascending(x => x.SequenceId);
         var indexModel = new CreateIndexModel<OutboxDocument>(indexKeys);
@@ -53,6 +57,15 @@ public class MongoDbOutboxRepository : IOutboxRepository
 
     public async Task<IEnumerable<OutboxTableRow>> Read(int batchSize, CancellationToken token = default)
     {
+        // Try to acquire the dispatcher lock
+        // For now let's assume that 10 seconds is a reasonable lock duration to be able to process a batch
+        var lockAcquired = await TryAcquireLock("outbox_dispatcher", TimeSpan.FromSeconds(10), token);
+        if (!lockAcquired)
+        {
+            // Couldn't acquire lock - another instance is dispatching
+            return [];
+        }
+
         var filter = Builders<OutboxDocument>.Filter.Empty;
         var sort = Builders<OutboxDocument>.Sort.Ascending(x => x.SequenceId);
 
@@ -71,6 +84,43 @@ public class MongoDbOutboxRepository : IOutboxRepository
         await _collection.DeleteManyAsync(deleteFilter, token).ConfigureAwait(false);
 
         return documents.Select(CreateOutboxTableRow);
+    }
+
+    private async Task<bool> TryAcquireLock(string lockName, TimeSpan lockDuration, CancellationToken token = default)
+    {
+        var now = DateTime.UtcNow;
+        var expiresAt = now.Add(lockDuration);
+
+        var filter = Builders<LockDocument>.Filter.And(
+            Builders<LockDocument>.Filter.Eq(x => x.Id, lockName),
+            Builders<LockDocument>.Filter.Or(
+                Builders<LockDocument>.Filter.Eq(x => x.LockOwner, null),        // No current lock
+                Builders<LockDocument>.Filter.Lt(x => x.ExpiresAt, now)          // Expired lock
+            )
+        );
+
+        var update = Builders<LockDocument>.Update
+            .Set(x => x.LockOwner, _instanceId)
+            .Set(x => x.ExpiresAt, expiresAt);
+
+        var options = new FindOneAndUpdateOptions<LockDocument>
+        {
+            IsUpsert = true,                    // Create if doesn't exist
+            ReturnDocument = ReturnDocument.After
+        };
+
+        try
+        {
+            var result = await _lockCollection.FindOneAndUpdateAsync(filter, update, options, token);
+
+            // We got the lock if the returned document has our instance ID
+            return result?.LockOwner == _instanceId;
+        }
+        catch (MongoException)
+        {
+            // Lock contention or other MongoDB error
+            return false;
+        }
     }
 
     public ITransactionScope BeginTransaction()
