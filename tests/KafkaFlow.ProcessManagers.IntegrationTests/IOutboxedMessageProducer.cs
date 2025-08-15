0a753ed48c4f9c7172cd90b5ac971fbdfbc04a3e
Alexey Raga <alexey.raga@gmail.com>
2025-08-15 16:32:17 +1000

    Fix tests

---
+++ tests/KafkaFlow.ProcessManagers.IntegrationTests/IOutboxedMessageProducer.cs
@@ -0,0 +1,9 @@
+namespace KafkaFlow.ProcessManagers.IntegrationTests;
+
+public interface IOutboxedMessageProducer
+{
+}
+
+public interface IUnOutboxedMessageProducer
+{
+}
--- tests/KafkaFlow.ProcessManagers.IntegrationTests/UserLifeCycle/UserLifeCycleProcess.cs
+++ tests/KafkaFlow.ProcessManagers.IntegrationTests/UserLifeCycle/UserLifeCycleProcess.cs
@@ -12,23 +12,20 @@ public sealed record UserApproved(Guid UserId);
 public sealed record UserAccessGranted(Guid UserId);
 
 // ReSharper disable once UnusedType.Global
-public class UserLifeCycleProcess(ILogger<UserLifeCycleProcess> logger, IMessageProducer<ITestMessageProducer> producer) : ProcessManager<TestState>,
+public class UserLifeCycleProcess(ILogger<UserLifeCycleProcess> logger, IMessageProducer<IOutboxedMessageProducer> producer) : ProcessManager<TestState>,
     IProcessMessage<UserRegistered>,
     IProcessMessage<UserAccessGranted>,
     IProcessMessage<UserApproved>
 {
-    private readonly ILogger _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    private readonly IMessageProducer<ITestMessageProducer> _producer = producer ?? throw new ArgumentNullException(nameof(producer));
-
     public Guid GetProcessId(UserRegistered message) => message.UserId;
     public Guid GetProcessId(UserAccessGranted message) => message.UserId;
     public Guid GetProcessId(UserApproved message) => message.UserId;
 
     public async Task Handle(IMessageContext context, UserRegistered message)
     {
-        _logger.LogInformation("Received message: {Message}", message);
-        await _producer.ProduceAsync(message.UserId.ToString(), new UserApproved(message.UserId));
-        await _producer.ProduceAsync(message.UserId.ToString(), new UserAccessGranted(message.UserId));
+        logger.LogInformation("Received message: {Message}", message);
+        await producer.ProduceAsync(message.UserId.ToString(), new UserApproved(message.UserId));
+        await producer.ProduceAsync(message.UserId.ToString(), new UserAccessGranted(message.UserId));
 
         var newState = new TestState(DateTimeOffset.UtcNow, ["UserRegistered"]);
         UpdateState(newState);
@@ -36,7 +33,7 @@ public class UserLifeCycleProcess(ILogger<UserLifeCycleProcess> logger, IMessage
 
     public async Task Handle(IMessageContext context, UserApproved message)
     {
-        _logger.LogInformation("Received message: {Message}", message);
+        logger.LogInformation("Received message: {Message}", message);
 
         await WithRequiredStateAsync(state =>
         {
@@ -48,7 +45,7 @@ public class UserLifeCycleProcess(ILogger<UserLifeCycleProcess> logger, IMessage
 
     public Task Handle(IMessageContext context, UserAccessGranted message)
     {
-        _logger.LogInformation("Received message: {Message}", message);
+        logger.LogInformation("Received message: {Message}", message);
         FinishProcess();
         return Task.CompletedTask;
     }
